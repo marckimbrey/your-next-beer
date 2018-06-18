794a47b15eac2b9a95d61810bcf86c1390326e24
marckimbrey <marckimbrey@gmail.com>
2018-06-18 11:09:01 +0100

    Added site header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const styles = {
+  container: {
+    display: 'flex',
+    listStyle: 'none',
+    justifyContent: 'space-between'
+  },
+  appBar: {
+  },
+  header: {
+     marginRight: 'auto'
+  },
+  navLink: {
+  },
+  button: {
+    marginRight: 20
+    }
+}
+ export default () => {
+   return (
+       <AppBar>
+         <ul style={styles.container}>
+         <li style={styles.header}><Typography variant="headline" xs={6} color="inherit" >
+           Your Next Beer
+         </Typography></li>
+          <li><NavLink style={styles.navLink} xs={3} to="../login">
+            <Button style={styles.button}>Login</Button>
+          </NavLink></li>
+          <li><NavLink style={styles.navLink} xs={3} to="../createPoll">
+            <Button style={styles.button} >create poll</Button>
+          </NavLink></li>
+        </ul>
+      </AppBar>
+   )
+ }
